2832915de5a474906c08606e1a5b1f40ee30f27d
Haby Randall <sow.haby@gmail.com>
2017-07-17 16:15:18 -0700

    More redux boiler plate

--- src/app.js
+++ src/app.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
 import Header from './components/Header';
 import PlacePicker from './components/PlacePicker';
 import AutoComplete from  './components/AutoComplete';
 
-const App = () => (
+const App = () => {
+  return (
+    <Provider store={createStore(reducers)}>
       <View style={{ flex: 1 }}>
-      <Header headerText={'ParkFinder'}/>
-      <PlacePicker />
-      <AutoComplete />
+        <Header headerText={'ParkFinder'}/>
+        <PlacePicker />
+        <AutoComplete />
       </View>
+      </Provider>
   );
+};
 
   export default App;
